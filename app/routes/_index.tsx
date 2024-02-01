bdc0502cebd2c10b8ed46f89947f73429caa5149
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-01 16:53:40 +0900

    feat(routes/index): update Index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,34 +6,15 @@ import LoginForm from '../features/auth/components/LoginForm';
 import LogoutButton from '../features/auth/components/LogoutButton';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
+  return [{ title: 'Raimei' }, { name: 'description', content: 'Raimei is My blog editor.' }];
 };
 
 export default function Index() {
   const session = useAtomValue(userSessionAtom);
 
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-
+    <div>
+      <h1>Raimei</h1>
       {session == null ? <LoginForm onSubmit={() => {}} /> : <></>}
       {session != null ? <LogoutButton onClick={() => {}} /> : <></>}
     </div>
