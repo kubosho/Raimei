0b48459362b07f9753a0a1f02833348f7a8298d7
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-03 15:41:59 +0900

    feat(app): implement the TextEditor component

---
+++ app/features/editor/components/TextEditor.tsx
@@ -0,0 +1,53 @@
+import CodePkg from '@lexical/code';
+import LinkPkg from '@lexical/link';
+import ListPkg from '@lexical/list';
+import LexicalMarkdownPkg from '@lexical/markdown';
+import RichTextPkg from '@lexical/rich-text';
+import LexicalComposerPkg from '@lexical/react/LexicalComposer.js';
+import type { InitialConfigType } from '@lexical/react/LexicalComposer.js';
+import ContentEditablePkg from '@lexical/react/LexicalContentEditable.js';
+import HistoryPluginPkg from '@lexical/react/LexicalHistoryPlugin.js';
+import MarkdownShortcutPluginPkg from '@lexical/react/LexicalMarkdownShortcutPlugin.js';
+import RichTextPluginPkg from '@lexical/react/LexicalRichTextPlugin.js';
+import type { LexicalEditor } from 'lexical';
+
+import AppErrorBoundary from '../../../common_components/AppErrorBoundary';
+
+const { CodeNode } = CodePkg;
+const { LinkNode } = LinkPkg;
+const { ListNode, ListItemNode } = ListPkg;
+const { HeadingNode, QuoteNode } = RichTextPkg;
+
+const { TRANSFORMERS } = LexicalMarkdownPkg;
+const { LexicalComposer } = LexicalComposerPkg;
+const { ContentEditable } = ContentEditablePkg;
+const { RichTextPlugin } = RichTextPluginPkg;
+const { HistoryPlugin } = HistoryPluginPkg;
+const { MarkdownShortcutPlugin } = MarkdownShortcutPluginPkg;
+
+const EDITOR_NODES = [CodeNode, HeadingNode, LinkNode, ListNode, ListItemNode, QuoteNode];
+const initialConfig: InitialConfigType = {
+  namespace: 'RaimeiEditor',
+  nodes: EDITOR_NODES,
+  onError: (error: Error, editor: LexicalEditor) => {
+    console.error(error, editor);
+  },
+};
+
+export default function TextEditor() {
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="relative">
+        <RichTextPlugin
+          contentEditable={<ContentEditable className="focus:outline-none" />}
+          placeholder={
+            <p className="absolute left-0 pointer-events-none select-none text-gray-500 top-0">Write the texts</p>
+          }
+          ErrorBoundary={AppErrorBoundary}
+        />
+      </div>
+      <HistoryPlugin />
+      <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+    </LexicalComposer>
+  );
+}
