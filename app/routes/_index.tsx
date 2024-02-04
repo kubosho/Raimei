84c9ce0e5b3b1b341032039c333764c458166588
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-04 16:49:03 +0900

    feat(routes/index): introduce login/logout features to the top page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,26 +1,37 @@
 import type { MetaFunction } from '@remix-run/node';
+import { useNavigate } from '@remix-run/react';
 import { useAtomValue } from 'jotai/react';
+import { useCallback } from 'react';
 
 import { ThunderSvg } from '../assets/components/ThunderSvg';
+import LoginButton from '../features/auth/components/LoginButton';
 import { userSessionAtom } from '../features/auth/atoms/user_session_atom';
-import LoginForm from '../features/auth/components/LoginForm';
-import LogoutButton from '../features/auth/components/LogoutButton';
 import TextEditor from '../features/editor/components/TextEditor';
 import TitleEditor from '../features/editor/components/TitleEditor';
+import AccountMenu from '../features/navigation/AccountMenu';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Raimei' }, { name: 'description', content: 'Raimei is My blog editor.' }];
 };
 
 export default function Index() {
+  const navigate = useNavigate();
+
   const session = useAtomValue(userSessionAtom);
+
+  const handleClickLoginButton = useCallback(() => {
+    navigate({ pathname: '/auth/login' });
+  }, [navigate]);
 
   return (
     <>
-      <h1 className="items-center flex max-w-screen-md mx-auto text-gray-900">
-        <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
-        <span className="ml-1">Raimei</span>
-      </h1>
+      <header className="flex justify-between max-w-screen-md mx-auto">
+        <h1 className="items-center flex text-gray-900">
+          <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
+          <span className="ml-1">Raimei</span>
+        </h1>
+        {session == null ? <LoginButton onClick={handleClickLoginButton} /> : <AccountMenu />}
+      </header>
       <main>
         <div className="max-w-screen-md mx-auto">
           <TitleEditor />
@@ -28,8 +39,6 @@ export default function Index() {
         <div className="max-w-screen-md mt-10 mx-auto">
           <TextEditor />
         </div>
-        {session == null ? <LoginForm onSubmit={() => {}} /> : <></>}
-        {session != null ? <LogoutButton onClick={() => {}} /> : <></>}
       </main>
     </>
   );
