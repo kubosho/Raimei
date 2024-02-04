0ed9385000f0f0199ce4b2e47745de7129a70156
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-04 15:58:10 +0900

    feat(app/root): set auth instance to authAtom

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,7 @@ import { Database } from '../types/database.types';
 
 import { useInstance } from './common_hooks/use_instance';
 import { Auth } from './features/auth';
+import { authAtom } from './features/auth/atoms/auth_atom';
 import { userSessionAtom } from './features/auth/atoms/user_session_atom';
 import stylesheet from './tailwind.css';
 
@@ -52,9 +53,11 @@ export default function App() {
   const { env, session } = useLoaderData<typeof loader>();
   const { revalidate } = useRevalidator();
 
-  const setSession = useSetAtom(userSessionAtom);
   const supabase = useInstance(createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY));
   const auth = useInstance(new Auth(supabase.auth));
+
+  const setSession = useSetAtom(userSessionAtom);
+  const setAuth = useSetAtom(authAtom);
 
   const retriveCurrentSession = useCallback(async () => {
     const { data } = await auth.retriveSession();
@@ -64,7 +67,8 @@ export default function App() {
 
   useEffect(() => {
     retriveCurrentSession();
-  }, [retriveCurrentSession]);
+    setAuth(auth);
+  }, [auth, retriveCurrentSession, setAuth]);
 
   const serverAccessToken = session?.access_token;
 
