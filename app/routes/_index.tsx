3f7a3b32bcb2fafaecf5d0c2ae81dd4c1962bf8a
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-05 21:21:28 +0900

    feat(routes): use Header component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,12 +3,12 @@ import { useNavigate } from '@remix-run/react';
 import { useAtomValue } from 'jotai/react';
 import { useCallback } from 'react';
 
-import { ThunderSvg } from '../assets/components/ThunderSvg';
 import LoginButton from '../features/auth/components/LoginButton';
 import { userSessionAtom } from '../features/auth/atoms/user_session_atom';
 import TextEditor from '../features/editor/components/TextEditor';
 import TitleEditor from '../features/editor/components/TitleEditor';
 import AccountMenu from '../features/navigation/AccountMenu';
+import Header from '../features/navigation/Header';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Raimei' }, { name: 'description', content: 'Raimei is My blog editor.' }];
@@ -25,13 +25,7 @@ export default function Index() {
 
   return (
     <>
-      <header className="flex justify-between max-w-screen-md mx-auto">
-        <h1 className="items-center flex text-gray-900">
-          <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
-          <span className="ml-1">Raimei</span>
-        </h1>
-        {session == null ? <LoginButton onClick={handleClickLoginButton} /> : <AccountMenu />}
-      </header>
+      <Header>{session == null ? <LoginButton onClick={handleClickLoginButton} /> : <AccountMenu />}</Header>
       <main>
         <div className="max-w-screen-md mx-auto">
           <TitleEditor />
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from '@remix-run/react';
 
-import { ThunderSvg } from '../assets/components/ThunderSvg';
+import Header from '../features/navigation/Header';
 
 export default function AuthRoute() {
   return (
     <>
-      <header className="flex justify-between max-w-screen-md mx-auto">
-        <h1 className="items-center flex text-gray-900">
-          <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
-          <span className="ml-1">Raimei</span>
-        </h1>
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
