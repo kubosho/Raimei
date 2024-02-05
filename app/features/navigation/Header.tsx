0e059cbbe405c0a58e8067d34fc0c185fbb92a51
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-05 21:17:17 +0900

    feat(navigation): implement Header component

---
+++ app/features/navigation/Header.tsx
@@ -0,0 +1,22 @@
+import { Link } from '@remix-run/react';
+import type { ReactNode } from 'react';
+
+import { ThunderSvg } from '../../assets/components/ThunderSvg';
+
+interface Props {
+  children?: ReactNode;
+}
+
+export default function Header({ children }: Props) {
+  return (
+    <header className="flex items-center justify-between max-w-screen-md mx-auto py-4">
+      <h1 className="py-2">
+        <Link className="items-center flex text-gray-900" to={{ pathname: '/' }}>
+          <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
+          <span className="ml-1">Raimei</span>
+        </Link>
+      </h1>
+      {children}
+    </header>
+  );
+}
