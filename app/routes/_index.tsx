8ef0cf0761efc6c53a183b49eaf8210150f69410
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-10 01:09:17 +0900

    feat(routes/index): enable to post content to microCMS

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,18 @@
 import type { MetaFunction } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
 import { useAtomValue } from 'jotai/react';
-import { useCallback } from 'react';
 
 import LoginButton from '../features/auth/components/LoginButton';
 import { userSessionAtom } from '../features/auth/atoms/user_session_atom';
+import { bodyValueAsMarkdownAtom } from '../features/editor/atoms/body_value_as_markdown_atom';
+import { titleValueAtom } from '../features/editor/atoms/title_value_atom';
+import SubmitButton from '../features/editor/components/SubmitButton';
 import TextEditor from '../features/editor/components/TextEditor';
 import TitleEditor from '../features/editor/components/TitleEditor';
 import AccountMenu from '../features/navigation/AccountMenu';
 import Header from '../features/navigation/Header';
+import { microCmsClientAtom } from '../features/publish/atoms/micro_cms_client_atom';
+import { microCmsClientConfigAtom } from '../features/publish/atoms/micro_cms_client_config_atom';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Raimei' }, { name: 'description', content: 'Raimei is My blog editor.' }];
@@ -17,11 +21,29 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const navigate = useNavigate();
 
+  const bodyValueAsMarkdown = useAtomValue(bodyValueAsMarkdownAtom);
+  const titleValue = useAtomValue(titleValueAtom);
+  const microCmsClient = useAtomValue(microCmsClientAtom);
+  const microCmsClientConfig = useAtomValue(microCmsClientConfigAtom);
   const session = useAtomValue(userSessionAtom);
 
-  const handleClickLoginButton = useCallback(() => {
+  const handleClickLoginButton = () => {
     navigate({ pathname: '/auth/login' });
-  }, [navigate]);
+  };
+
+  const handleClickSubmitButton = () => {
+    if (microCmsClientConfig == null || microCmsClient == null) {
+      return;
+    }
+
+    microCmsClient.create({
+      endpoint: microCmsClientConfig.endpoint,
+      content: {
+        title: titleValue,
+        content: bodyValueAsMarkdown,
+      },
+    });
+  };
 
   return (
     <>
@@ -32,6 +54,9 @@ export default function Index() {
         </div>
         <div className="max-w-screen-md mt-10 mx-auto px-2 w-full">
           <TextEditor />
+        </div>
+        <div className="max-w-screen-md mt-10 mx-auto w-full">
+          <SubmitButton onClick={handleClickSubmitButton} />
         </div>
       </main>
     </>
