ef587cafdd0cbb7ad6d29ee089017526b0435074
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-10 01:08:48 +0900

    feat(TitleEditor): implement save the title feature

--- app/features/editor/components/TitleEditor.tsx
+++ app/features/editor/components/TitleEditor.tsx
@@ -1,4 +1,33 @@
+import { useAtom, useAtomValue } from 'jotai/react';
+import { useEffect, type ChangeEvent } from 'react';
+
+import { appStorageAtom } from '../../../storage/atoms/app_storage_atom';
+import { titleValueAtom } from '../atoms/title_value_atom';
+
 export default function TitleEditor() {
+  const appStorage = useAtomValue(appStorageAtom);
+
+  const [titleValue, setTitleValue] = useAtom(titleValueAtom);
+
+  const handleChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setTitleValue(value);
+    appStorage.set('titleEditorState', value);
+  };
+
+  const loadTitleEditorState = async () => {
+    return appStorage.get('titleEditorState');
+  };
+
+  useEffect(() => {
+    loadTitleEditorState().then((titleEditorState) => {
+      if (titleEditorState != null) {
+        setTitleValue(titleEditorState);
+      }
+    });
+  });
+
   return (
     <>
       <label className="sr-only text-gray-900" htmlFor="entry-title">
@@ -8,9 +37,10 @@ export default function TitleEditor() {
         type="text"
         name="entry-title"
         id="entry-title"
-        defaultValue=""
+        value={titleValue}
         className="focus:outline-none leading-relaxed placeholder-gray-500 text-2xl"
         placeholder="Write the title"
+        onChange={handleChangeValue}
       />
     </>
   );
