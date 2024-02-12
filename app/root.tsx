b473d22f8ecf832fbcfbcb4bebef4f6cbd4d3edf
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-12 12:27:01 +0900

    feat(app/root): fetch microCMS config in SSR

--- app/root.tsx
+++ app/root.tsx
@@ -17,9 +17,11 @@ import { useEffect } from 'react';
 import { Database } from '../types/database.types';
 
 import { useInstance } from './common_hooks/use_instance';
+import { supabaseClientAtom } from './databases/atom/supabase_client_atom';
 import { Auth } from './features/auth';
 import { authAtom } from './features/auth/atoms/auth_atom';
 import { userSessionAtom } from './features/auth/atoms/user_session_atom';
+import { microCmsClientConfigAtom } from './features/publish/atoms/micro_cms_client_config_atom';
 import stylesheet from './tailwind.css';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
@@ -32,7 +34,8 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const response = new Response();
   const cookies = parse(request.headers.get('Cookie') ?? '');
   const headers = new Headers();
-  const supabase = createServerClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY, {
+
+  const supabaseClient = createServerClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY, {
     cookies: {
       get(key) {
         return cookies[key];
@@ -45,14 +48,29 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       },
     },
   });
-  const auth = new Auth(supabase.auth);
+  const auth = new Auth(supabaseClient.auth);
 
-  const { data } = await auth.retriveSession();
+  const { data } = await supabaseClient.from('micro_cms_configs').select();
+  const {
+    data: { session },
+  } = await auth.retriveSession();
+
+  const microCmsConfig = data
+    ?.filter((d) => d.supabase_user_id === session?.user.id)
+    .map((d) => {
+      return {
+        apiKey: d.api_key ?? '',
+        endpoint: d.endpoint ?? '',
+        serviceId: d.service_id ?? '',
+      };
+    })
+    .at(0);
 
   return json(
     {
       env,
-      session: data.session,
+      microCmsConfig,
+      session,
     },
     {
       headers: response.headers,
@@ -61,17 +79,16 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 };
 
 export default function App() {
-  const { env, session } = useLoaderData<typeof loader>();
+  const { env, microCmsConfig, session } = useLoaderData<typeof loader>();
   const { revalidate } = useRevalidator();
 
   const supabase = useInstance(createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY));
   const auth = useInstance(new Auth(supabase.auth));
 
   const setAuthAtom = useSetAtom(authAtom);
+  const setMicroCmsClientConfig = useSetAtom(microCmsClientConfigAtom);
+  const setSupabaseClientAtom = useSetAtom(supabaseClientAtom);
   const setUserSessionAtom = useSetAtom(userSessionAtom);
-
-  setUserSessionAtom(session);
-  setAuthAtom(auth);
 
   const serverAccessToken = session?.access_token;
 
@@ -88,6 +105,17 @@ export default function App() {
       subscription.unsubscribe();
     };
   }, [auth, revalidate, serverAccessToken]);
+
+  setAuthAtom(auth);
+  setSupabaseClientAtom(supabase);
+
+  if (microCmsConfig != null) {
+    setMicroCmsClientConfig(microCmsConfig);
+  }
+
+  if (session != null) {
+    setUserSessionAtom(session);
+  }
 
   return (
     <html lang="ja">
