e620c1327c5491a5a6f70566f861c98c6b1a11b7
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-12 17:20:24 +0900

    feat(databases): implement to create supabase server client instance

---
+++ app/databases/supabase_server_client.ts
@@ -0,0 +1,36 @@
+import { createServerClient, parse, serialize } from '@supabase/ssr';
+import type { SupabaseClient } from '@supabase/supabase-js';
+
+import type { Database } from '../../types/database.types';
+
+interface Params {
+  request: Request;
+}
+
+export function createSupabaseServerClient({ request }: Params): SupabaseClient<Database> {
+  const { env } = process;
+
+  if (env.SUPABASE_ANON_KEY == null || env.SUPABASE_URL == null) {
+    throw new Error('SUPABASE_ANON_KEY or SUPABASE_URL are not set. Please set environment variables.');
+  }
+
+  const { SUPABASE_ANON_KEY, SUPABASE_URL } = env;
+  const cookies = parse(request.headers.get('Cookie') ?? '');
+  const headers = new Headers();
+
+  const supabaseClient = createServerClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    cookies: {
+      get(key) {
+        return cookies[key];
+      },
+      set(key, value, options) {
+        headers.append('Set-Cookie', serialize(key, value, options));
+      },
+      remove(key, options) {
+        headers.append('Set-Cookie', serialize(key, '', options));
+      },
+    },
+  });
+
+  return supabaseClient;
+}
