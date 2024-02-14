c5d8f152b93f43e1ae799d43cc234903c0114781
Shota Kubota <ta2@o2p.jp>
2024-02-15 01:39:50 +0900

    feat(databases): remove unnecessary atom

--- app/databases/atom/supabase_client_atom.ts
+++
@@ -1,6 +0,0 @@
-import type { SupabaseClient } from '@supabase/supabase-js';
-import { atom } from 'jotai/vanilla';
-
-import { Database } from '../../../types/database.types';
-
-export const supabaseClientAtom = atom<SupabaseClient<Database> | null>(null);
