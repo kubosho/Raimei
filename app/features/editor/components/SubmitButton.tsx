c395c2be4ff08cc581cdc9b402adb3cda003f919
Shota Kubota <ta2@o2p.jp>
2024-02-16 01:54:42 +0900

    feat(SubmitButton): tweak styles

--- app/features/editor/components/SubmitButton.tsx
+++ app/features/editor/components/SubmitButton.tsx
@@ -4,11 +4,7 @@ interface Props {
 
 export default function SubmitButton({ onClick }: Props) {
   return (
-    <button
-      type="button"
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={onClick}
-    >
+    <button type="button" className="border-2 border-yellow-500 px-4 py-1 rounded" onClick={onClick}>
       Submit
     </button>
   );
