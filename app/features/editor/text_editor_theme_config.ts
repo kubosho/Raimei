a812b48ff031bf0c08904926dfba5b6ca1662188
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-19 00:42:29 +0900

    feat(editor): tweak editor contents style

--- app/features/editor/text_editor_theme_config.ts
+++ app/features/editor/text_editor_theme_config.ts
@@ -2,12 +2,14 @@ import type { EditorThemeClasses } from 'lexical';
 
 export const textEditorThemeConfig: EditorThemeClasses = {
   heading: {
-    h1: 'leading-relaxed text-4xl',
-    h2: 'leading-relaxed text-3xl',
-    h3: 'leading-relaxed text-2xl',
-    h4: 'leading-relaxed text-xl',
+    h1: 'leading-relaxed mt-10 text-4xl',
+    h2: 'leading-relaxed mt-10 text-3xl',
+    h3: 'leading-relaxed mt-10 text-2xl',
+    h4: 'leading-relaxed mt-10 text-xl',
     h5: 'leading-relaxed text-lg',
     h6: 'leading-relaxed text-base',
   },
+  image: 'h-auto max-w-full rounded',
+  link: 'active:text-blue-600 decoration-1 text-blue-900 underline visited:text-blue-900',
   paragraph: 'leading-relaxed text-base',
 };
