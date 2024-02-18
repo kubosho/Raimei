4e4353b009f3754305542945c86bdc17f3c7e5fd
KUBOTA Shota <ta2+github@o2p.jp>
2024-02-18 21:53:23 +0900

    chore(auth_error_factory): update error message

--- app/features/auth/__mock__/auth_error_factory.ts
+++ app/features/auth/__mock__/auth_error_factory.ts
@@ -2,7 +2,7 @@ import type { AuthError } from '@supabase/supabase-js';
 import { Factory } from 'fishery';
 
 export const AuthErrorFactory = Factory.define<Omit<AuthError, '__isAuthError'>>(() => ({
-  message: 'invalid JWT',
+  message: 'invalid JWT: unable to parse or verify signature, token is expired by 1h00m10s',
   name: 'AuthError',
   status: 401,
 }));
