f6a5cd6b94b0366fb777518a28d73083f0d91eae
Shota Kubota <ta2@o2p.jp>
2024-02-21 12:53:52 +0900

    fix(databases): return new session if access token is different
    After the session expired on the Supabase side, the local access token was considere invalid and the access token was always updated.

--- app/databases/supabase_server_client.server.ts
+++ app/databases/supabase_server_client.server.ts
@@ -1,8 +1,10 @@
 import type { Session } from '@remix-run/node';
 import { createServerClient } from '@supabase/ssr';
+import { AuthError } from '@supabase/supabase-js';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
 import type { Database } from '../../types/database.types';
+import { getSession } from '../features/auth/cookie_session_storage.server';
 import type { SessionData, SessionFlashData } from '../features/auth/session_type';
 
 async function updateAccessToken({
@@ -11,7 +13,7 @@ async function updateAccessToken({
 }: {
   refreshToken: string;
   supabaseClient: SupabaseClient<Database>;
-}): Promise<string | null> {
+}): Promise<string> {
   const {
     data: { session },
     error,
@@ -19,8 +21,7 @@ async function updateAccessToken({
   const accessToken = session?.access_token;
 
   if (accessToken == null || error != null) {
-    console.error('Session expired. Please login again.');
-    return null;
+    return Promise.reject(new AuthError('Session expired, please login again', 401));
   }
 
   return accessToken;
@@ -32,11 +33,10 @@ async function verifyAccessToken({
 }: {
   accessToken: string;
   supabaseClient: SupabaseClient<Database>;
-}): Promise<string | null> {
+}): Promise<string> {
   const { error } = await supabaseClient.auth.getUser(accessToken);
   if (error != null) {
-    console.error(error.message);
-    return null;
+    return Promise.reject(error);
   }
 
   return accessToken;
@@ -51,12 +51,9 @@ async function fetchAvailableAccessToken({
   refreshToken: string;
   supabaseClient: SupabaseClient<Database>;
 }): Promise<string | null> {
-  const verifiedAccessToken = await verifyAccessToken({ accessToken, supabaseClient });
-  if (verifiedAccessToken != null) {
-    return verifiedAccessToken;
-  }
-
-  return await updateAccessToken({ refreshToken, supabaseClient });
+  return await verifyAccessToken({ accessToken, supabaseClient })
+    .catch(() => updateAccessToken({ refreshToken, supabaseClient }))
+    .catch(() => null);
 }
 
 export async function createSupabaseServerClient(session?: Session<SessionData, SessionFlashData>): Promise<{
@@ -105,7 +102,14 @@ export async function createSupabaseServerClient(session?: Session<SessionData, 
       supabaseClient,
     };
   } else if (availableAccessToken !== storedAccessToken) {
-    session.set('accessToken', availableAccessToken);
+    const userId = session.get('userId');
+    const newSession = await getSession();
+    newSession.set('accessToken', availableAccessToken);
+    newSession.set('refreshToken', storedRefreshToken);
+    if (userId != null) {
+      newSession.set('userId', userId);
+    }
+
     const supabaseClient = createServerClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
       cookies: {},
       global: {
@@ -116,7 +120,7 @@ export async function createSupabaseServerClient(session?: Session<SessionData, 
     });
 
     return {
-      session,
+      session: newSession,
       supabaseClient,
     };
   }
