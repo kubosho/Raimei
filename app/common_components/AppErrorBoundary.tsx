d6d66152e6be3e952381b3da0a27ad3ddaf7d77e
Shota Kubota <ta2@o2p.jp>
2024-03-04 11:13:48 +0900

    feat(AppErrorBoundary): change can be passed children props to AppErrorBoundry

--- app/common_components/AppErrorBoundary.tsx
+++ app/common_components/AppErrorBoundary.tsx
@@ -1,10 +1,19 @@
 import { useCallback } from 'react';
+import type { ReactNode } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
-export default function AppErrorBoundary() {
+interface Props {
+  children: ReactNode;
+}
+
+export default function AppErrorBoundary({ children }: Props) {
   const handleError = useCallback(() => {
     console.error('something went wrong');
   }, []);
 
-  return <ErrorBoundary fallback={<p>something went wrong...</p>} onError={handleError} />;
+  return (
+    <ErrorBoundary fallback={<p>something went wrong...</p>} onError={handleError}>
+      {children}
+    </ErrorBoundary>
+  );
 }
