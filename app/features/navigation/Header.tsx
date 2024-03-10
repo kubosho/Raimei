4a78867c9fcac1ff4bda6305572db0955dce5856
Shota Kubota <ta2@o2p.jp>
2024-03-11 00:55:33 +0900

    feat(navigation/header): implement useHeading option

--- app/features/navigation/Header.tsx
+++ app/features/navigation/Header.tsx
@@ -5,17 +5,30 @@ import { ThunderSvg } from '../../assets/components/ThunderSvg';
 
 interface Props {
   children?: ReactNode;
+  useHeading?: boolean;
 }
 
-export default function Header({ children }: Props) {
+function HeaderLink(): JSX.Element {
+  return (
+    <Link className="flex items-center pr-1 text-gray-900" to={{ pathname: '/' }}>
+      <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
+      <span className="ml-1">Raimei</span>
+    </Link>
+  );
+}
+
+export default function Header({ children, useHeading = true }: Props): JSX.Element {
   return (
     <header className="flex items-center justify-between max-w-screen-md mx-auto px-2 py-4 w-full">
-      <h1 className="py-2">
-        <Link className="flex items-center pr-1 text-gray-900" to={{ pathname: '/' }}>
-          <ThunderSvg alt="" className="fill-yellow-500 h-4 w-4" />
-          <span className="ml-1">Raimei</span>
-        </Link>
-      </h1>
+      {useHeading ? (
+        <h1 className="py-2">
+          <HeaderLink />
+        </h1>
+      ) : (
+        <div className="py-2">
+          <HeaderLink />
+        </div>
+      )}
       {children}
     </header>
   );
