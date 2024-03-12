882070e8c8033292a6b2f3c8eec5559e2fdac17a
Shota Kubota <ta2@o2p.jp>
2024-03-13 01:04:48 +0900

    feat(routes/index): change page appearance

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,10 +46,7 @@ function LoggedOutIndex(): JSX.Element {
       <main className="flex items-center mx-2">
         <div className="bg-yellow-50 border border-yellow-500 flex items-center justify-between max-w-screen-md mx-auto rounded px-8 py-10 w-full">
           <h1 className="flex font-thin items-center text-4xl">Just Writing.</h1>
-          <Link
-            className="bg-yellow-500 border-2 border-yellow-500 px-4 py-1 rounded text-slate-900"
-            to={{ pathname: '/login' }}
-          >
+          <Link className="bg-yellow-500 px-4 py-2 rounded text-slate-900" to={{ pathname: '/login' }}>
             Login
           </Link>
         </div>
@@ -64,11 +61,13 @@ function LoggedInIndex({ contents }: { contents: MicroCmsApiSchema[] }): JSX.Ele
       <Header>
         <AccountMenu hasSession={true} />
       </Header>
-      <main className="pb-16">
-        <ul className="max-w-screen-md mx-auto px-2">
-          <li>
-            <Link to="/entries/new">Create new enrty</Link>
-          </li>
+      <main className="mt-4">
+        <div className="max-w-screen-md mx-auto px-2">
+          <Link className="bg-yellow-500  leading-relaxed px-4 py-2 rounded text-slate-900" to="/entries/new">
+            Create new enrty
+          </Link>
+        </div>
+        <ul className="max-w-screen-md mt-8 mx-auto px-2">
           {contents.map(({ id, title }) => (
             <li key={id} className="my-4">
               <Link to={`/entries/${id}`}>{title}</Link>
