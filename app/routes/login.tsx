70c4bce45c2e5d39f16441f41d72bd97d4f7ff35
Shota Kubota <ta2@o2p.jp>
2024-03-13 01:04:57 +0900

    feat(routes/login): change page appearance

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -46,12 +46,12 @@ export const meta: MetaFunction = () => {
 export default function Login() {
   return (
     <>
-      <Header />
-      <main className="flex items-center">
-        <section className="max-w-screen-md mx-auto px-2 w-full">
-          <h2 className="leading-relaxed text-3xl">Login</h2>
+      <Header useHeading={false} />
+      <main>
+        <section className="max-w-screen-md mx-auto px-2">
+          <h1 className="leading-relaxed text-3xl">Login</h1>
           <div className="bg-yellow-50 border border-yellow-500 max-w-screen-md mt-8 mx-auto rounded px-8 py-10">
-            <Form action="/login" method="post" className="flex flex-col w-full">
+            <Form action="/login" method="post" className="flex flex-col">
               <label htmlFor="email">Email</label>
               <input
                 className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
@@ -75,10 +75,7 @@ export default function Login() {
                 defaultValue=""
                 autoComplete="current-password"
               />
-              <button
-                type="submit"
-                className="bg-yellow-500 border-2 border-yellow-500 mt-10 px-4 py-1 rounded text-slate-900"
-              >
+              <button type="submit" className="bg-yellow-500 mt-10 px-4 py-2 rounded text-slate-900">
                 Login
               </button>
             </Form>
