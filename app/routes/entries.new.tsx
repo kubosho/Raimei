d65d8c7c04fdc2b4ee5e10a0aac8746868e34e68
Shota Kubota <ta2@o2p.jp>
2024-05-06 13:10:10 +0900

    refactor(entries.new): clarify data flow for contents post

--- app/routes/entries.new.tsx
+++ app/routes/entries.new.tsx
@@ -1,32 +1,56 @@
 import type { KvsEnvStorage } from '@kvs/env/lib/share';
 import { json } from '@remix-run/node';
-import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import { useAtom, useAtomValue } from 'jotai/react';
-import { createClient } from 'microcms-js-sdk';
+import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node';
+import { useFetcher, useLoaderData } from '@remix-run/react';
+import { useAtom } from 'jotai/react';
 import { useCallback, useEffect, useState } from 'react';
 import { ClientOnly } from 'remix-utils/client-only';
 
+import { cmsContentsRepository } from '../cms/cms_contents_repository';
 import Loading from '../components/Loading';
-import { getSession } from '../features/auth/cookie_session_storage.server';
-import { bodyValueAsMarkdownAtom } from '../features/editor/atoms/body_value_as_markdown_atom';
 import { bodyValueAtom } from '../features/editor/atoms/body_value_atom';
 import { titleValueAtom } from '../features/editor/atoms/title_value_atom';
 import Editor from '../features/editor/components/Editor';
 import SubmitButton from '../features/editor/components/SubmitButton';
 import AccountMenu from '../features/navigation/AccountMenu';
 import Header from '../features/navigation/Header';
-import { microCmsClientAtom } from '../features/publish/atoms/micro_cms_client_atom';
-import { microCmsClientConfigAtom } from '../features/publish/atoms/micro_cms_client_config_atom';
 import { getEditorStorageInstance, initializeEditorStorageInstance } from '../local_storage/editor_storage.client';
 import type { EditorStorageSchema } from '../local_storage/editor_storage_schema.client';
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const session = await getSession(request.headers.get('Cookie'));
-  const userId = session.get('userId');
-  const hasSession = userId != null;
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const repository = cmsContentsRepository();
+  if (repository == null) {
+    return null;
+  }
 
-  return json({ hasSession });
+  const body = await request.formData();
+  const title = body.get('title') as string;
+  const contents = body.get('contents') as string;
+  const slug = body.get('slug') as string;
+
+  const res = await repository.create(
+    {
+      title,
+      body: contents,
+      slug,
+    },
+    {},
+  );
+
+  return res;
+};
+
+export const loader = async () => {
+  const repository = cmsContentsRepository();
+  if (repository == null) {
+    return json({
+      hasSession: false,
+    });
+  }
+
+  return json({
+    hasSession: true,
+  });
 };
 
 export const meta: MetaFunction = () => {
@@ -34,29 +58,13 @@ export const meta: MetaFunction = () => {
 };
 
 export default function EntryNew() {
+  const fetcher = useFetcher();
   const { hasSession } = useLoaderData<typeof loader>();
 
   const [storage, setStorage] = useState<KvsEnvStorage<EditorStorageSchema> | null>(null);
 
-  const [microCmsClient, setMicroCmsClient] = useAtom(microCmsClientAtom);
-
-  const bodyAsMarkdown = useAtomValue(bodyValueAsMarkdownAtom);
   const [bodyValue, setBodyValue] = useAtom(bodyValueAtom);
   const [titleValue, setTitleValue] = useAtom(titleValueAtom);
-  const microCmsClientConfig = useAtomValue(microCmsClientConfigAtom);
-
-  const createMicroCmsClient = useCallback(() => {
-    const config = microCmsClientConfig;
-    if (config == null || config.apiKey === '' || config.serviceId === '') {
-      return;
-    }
-
-    const client = createClient({
-      apiKey: config.apiKey,
-      serviceDomain: config.serviceId,
-    });
-    setMicroCmsClient(client);
-  }, [microCmsClientConfig, setMicroCmsClient]);
 
   const initializeEditorState = useCallback(async () => {
     await initializeEditorStorageInstance();
@@ -97,23 +105,18 @@ export default function EntryNew() {
   );
 
   const handleClickSubmitButton = useCallback(async () => {
-    if (microCmsClientConfig == null || microCmsClient == null) {
-      return;
-    }
-
-    await microCmsClient.create({
-      endpoint: microCmsClientConfig.endpoint,
-      content: {
-        content: bodyAsMarkdown,
+    fetcher.submit(
+      {
+        contents: bodyValue,
         title: titleValue,
       },
-    });
-  }, [bodyAsMarkdown, microCmsClient, microCmsClientConfig, titleValue]);
+      { method: 'POST' },
+    );
+  }, [bodyValue, fetcher, titleValue]);
 
   useEffect(() => {
-    createMicroCmsClient();
     initializeEditorState();
-  }, [createMicroCmsClient, initializeEditorState]);
+  }, [initializeEditorState]);
 
   if (!hasSession) {
     return (
