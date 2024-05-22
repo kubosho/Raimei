7aa0bae66353e06fb49e689c5dad3701372d59a7
Shota Kubota <ta2@o2p.jp>
2024-05-22 11:02:03 +0900

    feat(settings): redirect to /login when logged out

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { Form, useActionData, useLoaderData } from '@remix-run/react';
 import type { MetaFunction } from '@remix-run/react';
@@ -15,10 +15,7 @@ import { fetchMicroCmsClientConfig } from '../features/publish/micro_cms_client_
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userData = await authenticator.isAuthenticated(request);
   if (userData == null) {
-    return json({
-      hasSession: false,
-      microCmsConfig: null,
-    });
+    return redirect('/login');
   }
 
   const supabaseClient = createSupabaseServerClient({ session: userData.session });
@@ -106,52 +103,48 @@ export default function SettingsMicroCms(): JSX.Element {
       <main>
         <section className="max-w-screen-md mx-auto px-2">
           <h2 className="leading-relaxed text-3xl">Settings</h2>
-          {!hasSession ? (
-            <p>Please log in to view settings.</p>
-          ) : (
-            <Form
-              action="/settings"
-              method="post"
-              className="bg-yellow-50 border border-yellow-500 flex flex-col max-w-screen-md mt-8 mx-auto rounded px-8 py-10"
-            >
-              <label htmlFor="serviceId">microCMS service ID</label>
-              <input
-                className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
-                type="text"
-                name="serviceId"
-                id="serviceId"
-                defaultValue={microCmsConfig?.serviceId}
-                autoCapitalize="none"
-              />
-              <label className="mt-10" htmlFor="endpoint">
-                microCMS API Endpoint
-              </label>
-              <input
-                className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
-                type="text"
-                name="endpoint"
-                id="endpoint"
-                defaultValue={microCmsConfig?.endpoint}
-                autoCapitalize="none"
-                autoCorrect="off"
-              />
-              <label className="mt-10" htmlFor="apiKey">
-                microCMS API key
-              </label>
-              <input
-                className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
-                type="text"
-                name="apiKey"
-                id="apiKey"
-                defaultValue={microCmsConfig?.apiKey}
-                autoCapitalize="none"
-                autoCorrect="off"
-              />
-              <button type="submit" className="bg-yellow-500 mt-10 px-4 py-2 rounded text-slate-900">
-                Submit
-              </button>
-            </Form>
-          )}
+          <Form
+            action="/settings"
+            method="post"
+            className="bg-yellow-50 border border-yellow-500 flex flex-col max-w-screen-md mt-8 mx-auto rounded px-8 py-10"
+          >
+            <label htmlFor="serviceId">microCMS service ID</label>
+            <input
+              className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
+              type="text"
+              name="serviceId"
+              id="serviceId"
+              defaultValue={microCmsConfig?.serviceId}
+              autoCapitalize="none"
+            />
+            <label className="mt-10" htmlFor="endpoint">
+              microCMS API Endpoint
+            </label>
+            <input
+              className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
+              type="text"
+              name="endpoint"
+              id="endpoint"
+              defaultValue={microCmsConfig?.endpoint}
+              autoCapitalize="none"
+              autoCorrect="off"
+            />
+            <label className="mt-10" htmlFor="apiKey">
+              microCMS API key
+            </label>
+            <input
+              className="border-b-2 border-slate-500 focus:outline-none px-2 py-1 rounded-none"
+              type="text"
+              name="apiKey"
+              id="apiKey"
+              defaultValue={microCmsConfig?.apiKey}
+              autoCapitalize="none"
+              autoCorrect="off"
+            />
+            <button type="submit" className="bg-yellow-500 mt-10 px-4 py-2 rounded text-slate-900">
+              Submit
+            </button>
+          </Form>
         </section>
       </main>
     </>
