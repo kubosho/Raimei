fde14e4eccaa9a00008a8dc8c8f69398ed62370f
Shota Kubota <ta2@o2p.jp>
2024-05-27 23:54:38 +0900

    chore(vite): install global node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { vitePlugin } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
+
+installGlobals();
 
 export default defineConfig({
   plugins: [vitePlugin()],
